7de632735a3a9769a65d57b25b8c669f508a47d4
Anders Pearson <anders@columbia.edu>
2010-06-24 13:57:31 -0400

    put everything behind auth and styled the login page like the original

--- urls.py
+++ urls.py
@@ -6,6 +6,7 @@ import os.path
 admin.autodiscover()
 import staticmedia
 from mediamachine.machine.models import Video, Theme, Keyword
+from django.contrib.auth.decorators import login_required
 
 site_media_root = os.path.join(os.path.dirname(__file__),"media")
 
@@ -25,24 +26,24 @@ urlpatterns = patterns('',
                        # Example:
                        # (r'^mediamachine/', include('mediamachine.foo.urls')),
                        ('^accounts/',include('djangowind.urls')),
-                       ('^$','django.views.generic.simple.direct_to_template', {'template' : 'machine/index.html'}),
-                       url('^video/$','django.views.generic.list_detail.object_list',
+                       ('^$','machine.views.limited_direct_to_template', {'template' : 'machine/index.html'}),
+                       url('^video/$','machine.views.limited_object_list',
                         dict(video_info_dict,paginate_by=10, template_name='machine/video_list.html'),name='video_list'),
-                       url('^video/(?P<object_id>\d+)/$','django.views.generic.list_detail.object_detail',
+                       url('^video/(?P<object_id>\d+)/$','machine.views.limited_object_detail',
                         dict(video_info_dict,template_name='machine/video_detail.html'),name='video_detail'),
 
 
-                       url('^theme/$','django.views.generic.list_detail.object_list',
+                       url('^theme/$','machine.views.limited_object_list',
                         dict(theme_info_dict,template_name='machine/theme_list.html'),name='theme_list'),
-                       url('^theme/(?P<object_id>\d+)/$','django.views.generic.list_detail.object_detail',
+                       url('^theme/(?P<object_id>\d+)/$','machine.views.limited_object_detail',
                         dict(theme_info_dict,template_name='machine/theme_detail.html'),name='theme_detail'),
 
-                       url('^keyword/$','django.views.generic.list_detail.object_list',
+                       url('^keyword/$','machine.views.limited_object_list',
                         dict(keyword_info_dict,template_name='machine/keyword_list.html'),name='keyword_list'),
-                       url('^keyword/(?P<object_id>\d+)/$','django.views.generic.list_detail.object_detail',
+                       url('^keyword/(?P<object_id>\d+)/$','machine.views.limited_object_detail',
                         dict(keyword_info_dict,template_name='machine/keyword_detail.html'),name='keyword_detail'),
 
-                       (r'^databrowse/(.*)', databrowse.site.root),
+                       (r'^databrowse/(.*)', login_required(databrowse.site.root)),
                        (r'^admin/(.*)', admin.site.root),
                        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': site_media_root}),
                        (r'^uploads/(?P<path>.*)$','django.views.static.serve',{'document_root' : settings.MEDIA_ROOT}),
