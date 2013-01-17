79a1a30ceaf0e054bec35d785157f744ec48562f
Anders Pearson <anders@columbia.edu>
2013-01-17 16:39:39 -0500

    flake8ion

--- mediamachine/settings_shared.py
+++ mediamachine/settings_shared.py
@@ -5,31 +5,31 @@ import sys
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = ( )
+ADMINS = ()
 
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default' : {
-        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-        'NAME' : 'mediamachine',
-        'HOST' : '',
-        'PORT' : 5432,
-        'USER' : '',
-        'PASSWORD' : '',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mediamachine',
+        'HOST': '',
+        'PORT': 5432,
+        'USER': '',
+        'PASSWORD': '',
+    }
 }
 
 if 'test' in sys.argv:
     DATABASES = {
-        'default' : {
-            'ENGINE' : 'django.db.backends.sqlite3',
-            'NAME' : ':memory:',
-            'HOST' : '',
-            'PORT' : '',
-            'USER' : '',
-            'PASSWORD' : '',
-            }
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+            'HOST': '',
+            'PORT': '',
+            'USER': '',
+            'PASSWORD': '',
+        }
     }
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
@@ -57,7 +57,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.request',
-    )
+)
 
 MIDDLEWARE_CLASSES = (
     'django_statsd.middleware.GraphiteRequestTimingMiddleware',
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'mediamachine.urls'
 
 TEMPLATE_DIRS = (
     "/var/www/mediamachine/templates/",
-    os.path.join(os.path.dirname(__file__),"templates"),
+    os.path.join(os.path.dirname(__file__), "templates"),
 )
 
 INSTALLED_APPS = (
@@ -133,26 +133,33 @@ STATICMEDIA_MOUNTS = (
 
 # WIND settings
 
-AUTHENTICATION_BACKENDS = ('djangowind.auth.WindAuthBackend','django.contrib.auth.backends.ModelBackend',)
+AUTHENTICATION_BACKENDS = (
+    'djangowind.auth.WindAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
 WIND_BASE = "https://wind.columbia.edu/"
 WIND_SERVICE = "cnmtl_full_np"
 WIND_PROFILE_HANDLERS = ['djangowind.auth.CDAPProfileHandler']
-WIND_AFFIL_HANDLERS = ['djangowind.auth.AffilGroupMapper','djangowind.auth.StaffMapper','djangowind.auth.SuperuserMapper']
+WIND_AFFIL_HANDLERS = [
+    'djangowind.auth.AffilGroupMapper',
+    'djangowind.auth.StaffMapper',
+    'djangowind.auth.SuperuserMapper']
 WIND_STAFF_MAPPER_GROUPS = ['tlc.cunix.local:columbia.edu']
-WIND_SUPERUSER_MAPPER_GROUPS = ['anp8','jb2410','zm4','sbd12','egr2107','kmh2124','sld2131','amm8','mar227','ed2198']
+WIND_SUPERUSER_MAPPER_GROUPS = [
+    'anp8', 'jb2410', 'zm4', 'sbd12', 'egr2107', 'kmh2124',
+    'sld2131', 'amm8', 'mar227', 'ed2198']
 
 # TinyMCE settings
 
 TINYMCE_JS_URL = '/site_media/js/tiny_mce/tiny_mce.js'
 TINYMCE_JS_ROOT = 'media/js/tiny_mce'
 
-# if you set this to True, you may have to 
-# override TINYMCE_JS_ROOT with the full path on production
-TINYMCE_COMPRESSOR = False 
+TINYMCE_COMPRESSOR = False
 TINYMCE_SPELLCHECKER = True
 
-TINYMCE_DEFAULT_CONFIG = {'cols': 80, 
-                          'rows': 30,
-                          'plugins':'table,spellchecker,paste,searchreplace',
-                          'theme' : 'simple',
-                          }
+TINYMCE_DEFAULT_CONFIG = {
+    'cols': 80,
+    'rows': 30,
+    'plugins': 'table,spellchecker,paste,searchreplace',
+    'theme': 'simple',
+}
