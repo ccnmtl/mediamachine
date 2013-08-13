7682e0f3160c07369532addfe34ae9239f3de181
Anders Pearson <anders@columbia.edu>
2013-08-13 10:28:03 -0400

    increasing test coverage

--- mediamachine/machine/tests/test_models.py
+++ mediamachine/machine/tests/test_models.py
@@ -1,6 +1,55 @@
 from django.test import TestCase
+from mediamachine.machine.models import Resource
+from mediamachine.machine.models import Theme
+from mediamachine.machine.models import Keyword
+from mediamachine.machine.models import Video
 
 
-class DummyTest(TestCase):
-    def test_nothing(self):
-        self.assertTrue(True)
+class ResourceTest(TestCase):
+    def test_unicode(self):
+        r = Resource.objects.create()
+        self.assertEqual(str(r), "")
+
+    def test_get_absolute_url(self):
+        r = Resource.objects.create()
+        self.assertEqual(r.get_absolute_url(), "/resource/%d/" % r.id)
+
+
+class ThemeTest(TestCase):
+    def test_unicode(self):
+        r = Theme.objects.create()
+        self.assertEqual(str(r), "")
+
+    def test_get_absolute_url(self):
+        r = Theme.objects.create()
+        self.assertEqual(r.get_absolute_url(), "/theme/%d/" % r.id)
+
+
+class KeywordTest(TestCase):
+    def test_unicode(self):
+        r = Keyword.objects.create()
+        self.assertEqual(str(r), "")
+
+    def test_get_absolute_url(self):
+        r = Keyword.objects.create()
+        self.assertEqual(r.get_absolute_url(), "/keyword/%d/" % r.id)
+
+
+class VideoTest(TestCase):
+    def test_unicode(self):
+        r = Video.objects.create()
+        self.assertEqual(str(r), "")
+
+        r.scene = "foo"
+        self.assertEqual(str(r), ": foo")
+
+    def test_get_absolute_url(self):
+        r = Video.objects.create()
+        self.assertEqual(r.get_absolute_url(), "/video/%d/" % r.id)
+
+    def test_images(self):
+        r = Video.objects.create(sequence_count=2)
+        images = list(r.images())
+        self.assertEqual(
+            images,
+            [u'http://www.columbia.edu/itc/tc/cstudies/imagesequence/1.jpg'])
--- mediamachine/machine/tests/test_views.py
+++ mediamachine/machine/tests/test_views.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.test.client import Client
+from django.contrib.auth.models import User
+from mediamachine.machine.models import Video
 
 
 class SimpleViewTest(TestCase):
@@ -12,4 +14,34 @@ class SimpleViewTest(TestCase):
 
     def test_smoketest(self):
         result = self.c.get("/smoketest/")
+        self.assertEqual(result.status_code, 200)
+
+
+class LoggedInTest(TestCase):
+    def setUp(self):
+        self.u = User.objects.create(username="test")
+        self.u.set_password("test")
+        self.u.save()
+        self.c = Client()
+        self.c.login(username="test", password="test")
+
+    def test_index(self):
+        result = self.c.get("/")
+        self.assertEqual(result.status_code, 200)
+
+    def test_video_index(self):
+        result = self.c.get("/video/")
+        self.assertEqual(result.status_code, 200)
+
+    def test_theme_index(self):
+        result = self.c.get("/theme/")
+        self.assertEqual(result.status_code, 200)
+
+    def test_keyword_index(self):
+        result = self.c.get("/keyword/")
+        self.assertEqual(result.status_code, 200)
+
+    def test_video_detail(self):
+        v = Video.objects.create()
+        result = self.c.get(v.get_absolute_url())
         self.assertEqual(result.status_code, 200)
